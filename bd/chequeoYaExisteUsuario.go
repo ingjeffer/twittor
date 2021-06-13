7d6830eda6828834f88eaa16609ca658d19df166
Jefferson <j.martinezcuadros@dxc.com>
2021-06-13 17:43:57 -0500

    fix chequeoYaExisteUsuario

--- bd/chequeoYaExisteUsuario.go
+++ bd/chequeoYaExisteUsuario.go
@@ -8,12 +8,13 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+/*ChequeoYaExisteUsuario recibe un email de parámetro y chequea si ya está en la BD */
 func ChequeoYaExisteUsuario(email string) (models.Usuario, bool, string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	db := MongoCN.Database("twittor")
-	col := db.Collection("Usuarios")
+	col := db.Collection("usuarios")
 
 	condicion := bson.M{"email": email}
 
